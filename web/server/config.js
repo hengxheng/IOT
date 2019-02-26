dbd51a9ad1d827786aec5f8a7aa3b4e696b07376
Heng Zou <hengxheng@hotmail.com>
2019-02-27 09:17:02 +1100

    docker web init

--- web/server/config.js
+++
@@ -1,9 +0,0 @@
-export const config = {
-    device_url: "localhost",
-    device_port: 3001,
-    dbuser: "example",
-    dbpassword: "password",
-    dbname: "api",
-    dbcollection: "devices",
-    db: "mongodb://mongo:27017/"
-}
