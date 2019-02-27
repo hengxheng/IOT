ddaa7cd7f7aafa80e9045e4dedb86dcbc85c54ef
Heng Zou <hengxheng@hotmail.com>
2019-02-27 13:52:30 +1100

    Fixing routing

--- web/src/App.js
+++ web/src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import Header from './layout/Header';
 import Main from './layout/Main';
-import { BrowserRouter } from 'react-router-dom';
 export default class App extends React.Component{
     render(){
         return (
             <React.Fragment>
-                <BrowserRouter>
-                    <Header/>
-                    <Main/>
-                </BrowserRouter>
+                <Header/>
+                <Main/>
             </React.Fragment>
         )
     }
